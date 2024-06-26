bd506d166fbbfd797cd037f870830249bb3434c1
jacobxcoder <gillbatesdev@gmail.com>
2024-06-26 09:14:35 +0200

    feat: add copilot as a plugin

---
+++ lua/custom/configs/overrides.lua
@@ -0,0 +1,12 @@
+-- /custom directory does not seem to work - investigate why
+-- these plugins are not taken into account!
+
+local M = {}
+
+M.copilot = {
+  suggestion = {
+    auto_trigger = true,
+  },
+}
+
+return M
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,3 +1,13 @@
-return {
+-- /custom directory does not seem to work - investigate why
+-- these plugins are not taken into account!
 
+local overrides = require "custom.configs.overrides"
+
+local plugins = {
+  {
+    "zbirenbaum/copilot.lua",
+    opts = overrides.copilot,
+  },
 }
+
+return plugins
